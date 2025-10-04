791375d85bab21e020e1f672a1c2a92f7b9c3503
Eric Chukoltyansky <erik.chukoltyansky@gmail.com>
2025-10-04 18:49:09 +0300

    swagger build

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Location API')
+    .setDescription('API for locations management')
+    .setVersion('1.0')
+    .build();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
